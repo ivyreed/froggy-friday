d1a21a739cd3f48543e21e9d6757c71f306e377a
Ivy Reed <idcfu788@gmail.com>
2024-02-10 15:39:00 -0500

    added spikes, player death, level restart

---
+++ Assets/scripts/playerLife.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class playerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag=="trap")
+        {
+            die();
+        }
+
+    }
+    void die()
+    {
+        rb.bodyType=RigidbodyType2D.Static;
+    anim.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
