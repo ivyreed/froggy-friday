9cabd748b8268643bcb5db2aad6e6ae46d3b17fa
Ivy Reed <idcfu788@gmail.com>
2024-02-14 01:08:56 -0500

    better camera

--- Assets/scripts/cameraMovementScript.cs
+++ Assets/scripts/cameraMovementScript.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class cameraMovementScript : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+ 
 
-    // Update is called once per frame
-   private void Update()
-    {
-        transform.position=new Vector3(transform.position.x, player.position.y, transform.position.z);
-    }
+//This code is so the camera will trail behind the camera target giving the camera a bouncy feel
+private Vector3 offset = new Vector3(0f,0f,-10f);
+[SerializeField] private float smoothTime = .15f;
+private Vector3 velocity=Vector3.zero;
+
+[SerializeField] private Transform target;
+    private void Update()
+        {
+            Vector3 targetPosition = target.transform.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
 }
+
